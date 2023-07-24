16c178d66aece996120c8da46fdd90ed2cbaa679
T Mark <tmk1.tran@gmail.com>
2023-07-24 14:12:49 -0500

    started stretch goals and tested conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -73,6 +73,15 @@ if ( pets < allowedPets ) {
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+let mostPets;
+
+if ( pets > friendsPets ) {
+    mostPets = pets;
+} else {
+    mostPets = friendsPets;
+}
+
+console.log( mostPets );
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1

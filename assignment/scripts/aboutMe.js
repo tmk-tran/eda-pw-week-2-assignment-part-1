01e0318a179cfde05d97ba9fa58debc5b27fa365
T Mark <tmk1.tran@gmail.com>
2023-07-24 14:28:54 -0500

    tested stretch goal conditional statement for luckyNumber

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -14,7 +14,7 @@ let fullName = ( firstName + lastName );
     console.log( fullName );
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-let luckyNumber = 8
+let luckyNumber = 8;
 
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
@@ -89,6 +89,20 @@ console.log( mostPets );
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+switch ( luckyNumber ) {
+    case 1:
+        console.log( 'First is the worst' );
+        break;
+    case 2:
+        console.log( 'Second is the best' );
+        break;
+    case 3:
+        console.log( 'Third is the one with the polka dot dress' );
+        break;
+    default:
+    console.log( 'Luck is what happens when preparation meets opportunity' );
+}
+
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
 
